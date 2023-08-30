37710aca67de36974c16e2d3e06f14962240731b
unknown <merryskachr@gmail.com>
2023-08-30 16:18:22 +0800

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,13 @@
 import Express  from 'express';
 import taskRouter from './src/routes/task.route.js';
-// import { Server } from 'socket-io'
-// import http from 'http'
-
 
 const app = Express();
 app.use(Express.json())
-// const server = http.createServer(app)
-// const io = new Server(server)
-
-
 
 app.get('/',(req,res)=>{
   res.json('haloooo')
 })
 app.use(taskRouter)
-// io.on("connection", (socket)=>{
-//   socket.on("message", (data)=>{
-//     console.log(data)
-//   })
-// })
 
 app.listen(4000, ()=>{
   console.log('connected to server')
